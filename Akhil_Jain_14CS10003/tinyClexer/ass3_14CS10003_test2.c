d13b3c21236a3700a99aa226a70dd276f07e27d3
Arun Singh <arunsin997@gmail.com>
2018-10-09 13:21:01 +0530

    compiler other exes assignment

---
+++ Akhil_Jain_14CS10003/tinyClexer/ass3_14CS10003_test2.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<math.h>
+
+//single line comment
+
+/* multi
+ line
+
+ comment
+ ****/
+
+//keywords
+auto
+break
+case
+char
+const
+continue
+default
+do
+double
+else
+enum
+extern
+float
+for
+goto
+if
+inline
+int
+long
+register
+restrict
+return
+short
+signed
+sizeof
+static
+struct
+switch
+typedef
+union
+unsigned
+void
+volatile
+while
+_Bool
+_Complex
+_Imaginary
+
+
+int var=874;
+float y=512.6;
+y=.6;
+y=12e13;
+
+string greetings = "Welcome";
+"Testing String"
+
+char charvar = 'ok' ;
+9812 /* random integer */
+
+if(a==b){
+	printf("a equals b");
+
+}
+else {
+	printf("a is not equal to b\n");
+}
+
+
+'a' // testing char
+
+for(i=0;i<=n;i++){
+	printf("i");
+	j+=95;
+	k--;
+	m/=275;
+	j<<1;
+	....
+	{	.	., 
+		}
+		
+	a==b;
+}
+i=j*k;
+p=q+r;
+//punctuators
+[
+++
+/
+?
+=
+,
+]
+(
+{ } . ->
+* + - ~ !
+% << >> < > <= >=
+: ; ...
+*= /= %= += -= <<=
+#
+--
+)
+&
+==
+>>=
+!=
+&=
+^
+|
+^=
+&&
+||
+|=
+
+enum days{"mon","tues","wed","thur","fri","sun"};
+//different floats
+float a=19e+23
+21e+7
+4.
+.34
+98.445
+
+int x,i=1;
+loat z67=10.24;
+char c[] = "Hello";
+char y2b45dg = 'd';
+  for (i=1;i<10;i++) {
+    if(z67<20.48e-13) {
+printf ("%s",c);
+
